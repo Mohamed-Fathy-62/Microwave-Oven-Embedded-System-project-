c60385869996aa84f52ac0e150d2940abf53eda7
Mohamed Fathy <84096845+Mohamed-Fathy-62@users.noreply.github.com>
2022-05-25 22:19:06 +0200

    Delete Button.h

--- Button.h
+++
@@ -1,7 +0,0 @@
-#include "DigitalIO.h"
-
-void button_init(char portName, unsigned char pinNumber);
-
-unsigned char button_read(char portName, unsigned char pinNumber);
-
-void button_init_PUR(char portName, unsigned char pinNumber);
